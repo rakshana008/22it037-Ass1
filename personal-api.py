c85dc957f113e70b41e910531ccb1c00e042e908
rakshana-37 <rakshanaor@gmail.com>
2025-03-03 20:55:14 +0530

    Added personal-api.py

--- personal-api.py
+++ personal-api.py
@@ -1,1 +1,17 @@
+from flask import Flask 
+app = Flask(_name_)
+@app.route('/')
+def personal_details():
+    return 'Hello,World!'
 
+@app.route('/name')
+def get_name():
+    return 'RAKSHANA'
+@app.route('/regno')
+def get_regno():
+    return '22IT037'
+@app.route('/department')
+def get_dept():
+    return 'Information Technology'
+if _name_ == '_main_':
+    app.run(debug=True)
